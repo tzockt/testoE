4e76bb775b05d119fb2f248d0501a56253b90025
TZockt <info@tzockt.de>
2022-05-10 11:05:34 +0200

    Update MemeCommand

--- test/reddit-images.js
+++
@@ -1,7 +0,0 @@
-const { reddit } = require("reddit.images");
-
-
-reddit.FetchSubredditPost({ subreddit: 'NotMyJob' }).then((data) => {
-	console.log('FetchSubredditPost Example:');
-	console.log(data);
-});
